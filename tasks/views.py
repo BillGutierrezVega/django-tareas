a4030ad7f3e2195f3e1345962b00fdaf843e76f5
BillGutierrezVega <darckneck@hotmail.com>
2023-05-30 00:36:46 -0500

    lógica completa a falta de estilos

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.db import IntegrityError
 from .form import TaskForm
 from .models import Task
@@ -45,11 +47,19 @@ def signup(request):
             )
 
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, "tasks.html", {"tasks":tasks})
 
 
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, "tasks.html", {"tasks":tasks})
+
+
+@login_required
 def create_task(request):
     if request.method == "GET":
         return render(request, CREATE_TASK, {"form": TaskForm})
@@ -68,11 +78,44 @@ def create_task(request):
             )
 
 
+@login_required
 def task_detail(request, task_id):
-    tasks = get_object_or_404(Task, pk=task_id)
-    return render(request, "task_detail.html", {"tasks": tasks})
+    try:
+        if request.method == "GET":
+            tasks = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(instance=tasks)
+            return render(request, "task_detail.html", {"tasks": tasks, "form": form})
+        else:
+            tasks = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=tasks)
+            form.save()
+            return redirect("tasks")
+    except ValueError:
+        return render(
+            request,
+            'task_detail.html',
+            {"tasks": tasks, "form": TaskForm, "error": "Tarea no creada, ingrese valores válidos"},
+        )
 
 
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect("tasks")
+
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect("tasks")
+
+
+@login_required
 def cerrar_secion(request):
     logout(request)
     return redirect("home")
